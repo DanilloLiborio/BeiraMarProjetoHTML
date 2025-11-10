5dcf9bdcb6693e66a26e152c371cc5b536f8d4bc
DanilloLiborio <danillo.liborio@souunit.com.br>
2025-11-10 15:18:41 -0300

    Versão 1.0

---
+++ sidemenu/src/javascript/script.js
@@ -0,0 +1,16 @@
+var sideitem = document.querySelectorAll(".side_item");
+
+function selectlink(){
+    sideitem.forEach((item)=>
+        item.classList.remove('active')
+    )
+    this.classList.add('active')
+}
+
+sideitem.forEach((item)=>
+    item.addEventListener('click', selectlink)
+)
+
+function IrLogin(){
+    window.location.href = "../Login/login.html";
+}
